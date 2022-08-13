08d71a49858e816390a1ae374c40f7754b41eac5
Suresh <suresh@gmail.com>
2022-08-13 16:58:27 +0900

    Calculating area

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
-let a =4
-let b =5
-//arrow function to add
-let add = (a,b) => {
-    return a+b
+const radius =[10,20,30]
+// const area =radius.map(r=>Math.PI*r*r)
+// console.log(area)
+// const diameter = radius.map(r=>r*2)
+// console.log(diameter)
+// const circumference = radius.map(r=>2*Math.PI*r)
+// console.log(circumference)
+
+//  const calculate = function(radius,logic){
+//     // const output=[]
+//          return logic(radius)
+//  }
+//  console.log(calculate(radius,area))
+
+// // console.log(calculate(radius,diameter))
+// // console.log(calculate(radius,circumference))
+// console.log(calculate(radius,area))
+const area = function(radius){
+    return Math.PI*radius*radius
 }
-document.getElementById("demo").addEventListener("click", add)
+const circumference = function(radius){
+    return 2*Math.PI*radius
+}
+const diameter = function(radius){
+    return radius*2
+}
+const calculate = (function(radius,logic){
+    //map to calculate the array of radius
+
+    let output=[]
+    for(let i=0;i<radius.length;i++){
+
+        output.push(logic(radius[i]))
+    }
+    return output
+})
+
+console.log(calculate(radius,area))
+
